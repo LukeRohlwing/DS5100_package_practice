0f6884e0ab706658ac81aa8737c1d70108ab9cb4
LukeAlexander_Rohlwing <rohlwingluke@yahoo.com>
2023-07-31 11:24:11 -0400

    Participation

---
+++ practicepackage/module1.py
@@ -0,0 +1,6 @@
+def print_sum_2_2():
+    '''
+    Prints the sum of 2+2.
+    '''
+    print(4)
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='practice',
+    version='1.0.0',
+    url='https://github.com/LukeRohlwing/DS5100_package_practice',
+    author='Luke Rohlwing',
+    author_email='sqz4dh@virginia.edu',
+    description='Demo package',
+    packages=['practicepackage'],
+    install_requires = ['pandas']
+)
+
+
